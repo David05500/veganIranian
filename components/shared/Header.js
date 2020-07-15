a7b5cd0b46866b7da32659af93eb2da17930e15d
David Sarvasidze <sharvashidze.david@gmail.com>
2020-07-15 16:06:26 +0100

    h

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -10,9 +10,15 @@ const Header = () => {
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,800&display=swap" rel="stylesheet"></link>
       </Head>
       <nav className="max-width-1170 bg-gray-1000 border-btm  left-0 m-auto flex flex-col justify-center items-center z-150 py-4">
+
+        <div>
           <Link href="/">
             <h1 className='italic text-3xl lg:text-52xl text-black font-bold mb-8 pointer main-logo mt-8'>THE IRANIAN VEGAN</h1>
           </Link>
+          <div style={{backgroundSize: '100%', backgroundImage: 'url("logoBG.jpg")'}} >
+             
+          </div>
+        </div>
 
         <div className='flex w-4/5 lg:w-1/3 m-auto justify-around'> 
           <Link href="/recipes/blog">
