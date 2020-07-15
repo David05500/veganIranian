e85be91349614c7534be93c9e0ee61818796e47f
David Sarvasidze <sharvashidze.david@gmail.com>
2020-07-15 13:48:37 +0100

    h

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -94,7 +94,7 @@ const BlogPost = ({blogPost}) => {
                         {documentToReactComponents(post.recipeDescription, options)}
 
                         {/* Recipe Card */}
-                        <div ref={myRef} className='mb-8 lg:m-20  border-solid border-8 lg:border-15 border-white'>
+                        <div ref={myRef} className='mb-8 lg:m-20  border-solid border-2 lg:border-15 border-white shadow-sm'>
                             <div className='w-full bg-white'>
                                 <h1 className='py-10 text-center text-3xl font-medium'>{post.title}</h1>
                             </div>
@@ -161,7 +161,7 @@ const BlogPost = ({blogPost}) => {
                                     {documentToReactComponents(post.notes, options)}
                                 </div>
                             </div>
-                           
+                        
                             <div className='w-full flex bg-white p-4 lg:p-8'>
                                 <div className='w-1/3 flex justify-left lg:justify-center items-center'>
                                     <GrInstagram  size={60}/>
