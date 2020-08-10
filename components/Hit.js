67998547fd5c9aab33e354fef2cdc9b18c232d67
David Sarvasidze <sharvashidze.david@gmail.com>
2020-08-10 16:32:04 +0100

    added search

---
+++ components/Hit.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState, useRef} from 'react';
+import Link from 'next/link';
+
+
+const Hit = props => {
+    console.log(props.hit)
+  return (
+      <div>
+          Hello
+      </div>
+  )
+}
+
+
+export default Hit;
+
+
+
+
--- pages/recipes/blog.js
+++ pages/recipes/blog.js
@@ -6,7 +6,40 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Link from 'next/link';
 import _ from 'lodash';
 import Header from '../../components/shared/Header';
+import Hit from '../../components/Hit';
 import Head from 'next/head';
+
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox,  connectHits} from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch(
+  'KCP4G3STUF',
+  '70231e407e7e59d0ed63b92e2f0a8be7'
+);
+
+const Hits = data => {
+    let slug = '';
+    let hitData = '';
+    return _.map(data.hits, hit => {
+        slug = hit.fields.slug["en-US"];
+        hitData = _.find(data.blogPosts, { 'slug': slug});
+        return(
+            <div key={hitData.slug} className='lg:w-1/3 mb-8'>
+                <Link prefetch='ture'  href='/recipes/[slug]' as={`/recipes/${hitData.slug}/`}>
+                    <div className='card'>
+                        <div className='m-auto mb-4 relative pointer max-w-280px max-h-284px min-h-284px min-w-228px lg:max-w-228px   pointer hover:opacity-60 transform ease-in duration-100 ' style={{backgroundSize: '50%', backgroundImage:`url(${hitData.smallBlogPostImage.fields.file.url})`, backgroundRepeat:  'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover'}}>
+                        </div>
+                        <div >
+                            <h2 className='text-xs text-center px-4'>{hitData.title}</h2>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        )
+    })
+};
+
+const CustomHits = connectHits(Hits);
 
 const  Blog = ( props ) => {
     const [blogPosts, setBlogPosts] = useState([]);
@@ -24,27 +57,25 @@ const  Blog = ( props ) => {
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,800&display=swap" rel="stylesheet"></link>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
             </Head>
-            <div className='m-auto text-2xl bg-gray-primary'>
-                <Header />
-                <div className='max-width-735 px-4 lg:px-0 mx-auto lg:flex lg:flex-wrap mt-10'>
-                    {_.map(blogPosts, item => {
-                        return(
-                            <div key={item.slug} className='lg:w-1/3 mb-8'>
-                                <Link prefetch='ture'  href='/recipes/[slug]' as={`/recipes/${item.slug}/`}>
-                                    <div className='card'>
-                                        <div className='m-auto mb-4 relative pointer max-w-280px max-h-284px min-h-284px min-w-228px lg:max-w-228px   pointer hover:opacity-60 transform ease-in duration-100 ' style={{backgroundSize: '50%', backgroundImage:`url(${item.smallBlogPostImage.fields.file.url})`, backgroundRepeat:  'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover'}}>
-                                        </div>
-                                        <div >
-                                            <h2 className='text-xs text-center px-4'>{item.title}</h2>
-                                        </div>
-                                    </div>
-                                </Link>
+            {blogPosts != {} ? 
+                (<div className='m-auto text-2xl bg-gray-primary'>
+                    <Header />
+                    <div className='max-width-735 px-4 lg:px-0 mx-auto lg:flex lg:flex-wrap mt-10'>
+                        <InstantSearch
+                            indexName="dev_Iranian"
+                            searchClient={searchClient}
+                        >
+                            <div className='w-full mb-8 flex justify-center'>
+                                <SearchBox submit={<img src="" alt=""/>}/>
                             </div>
-                        )
-                    })}
+                            <CustomHits blogPosts={blogPosts}/>
+                        </InstantSearch>
+                    </div>
                 </div>
-       
-            </div>
+                )
+            :
+                (<div>Loading</div>)
+            }
         </div>
     )
 };
