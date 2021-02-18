1398ebac07aa1206f3d52b6c08c699052c5a7339
David Sarvasidze <sharvashidze.david@gmail.com>
2021-02-18 13:00:46 +0000

    fixing layout shift

--- pages/index.js
+++ pages/index.js
@@ -84,50 +84,52 @@ const  HomePage = () => {
         {/* <useProgressiveImage src={windowWidth > 430 ? `url(${homePagePic})` : `url(${mobileHomePagePic})`} placeholder='path/to/placeholder.jpg' /> */}
 
         <div className='h-screen w-screen  bg-no-repeat bg-103p bg-center flex justify-center items-center'  style={{backgroundSize: 'cover', backgroundImage: `url(${loaded})`}} >
-          <div style={{background: 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3))', pointerEvents: 'none',  width: '100vw', height: '100vh', position: 'absolute'}} />
-          <div className='flex flex-col justify-between pt-12 pb-28 lg:justify-center items-center h-screen '>
-            <h1 className='italic text-3xl lg:text-7xl text-white font-bold z-50 main-logo lg:text-spaceping-4 mt-28' style={{textShadow: '6px 6px 0px rgba(0,0,0,0.1)'}} >THE    IRANIAN    VEGAN</h1>
+          <div style={{background: 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3))', width: '100vw', height: '100vh'}} >
+            <div className='flex flex-col justify-between pt-12 pb-28 lg:justify-center items-center h-screen '>
+              <h1 className='italic text-3xl lg:text-7xl text-white font-bold z-50 main-logo lg:text-spaceping-4 mt-28' style={{textShadow: '6px 6px 0px rgba(0,0,0,0.1)'}} >THE    IRANIAN    VEGAN</h1>
 
-            <div className='flex flex-col justify-center items-center w-full'>
-              <div className='flex flex-col lg:flex-row items-center text-white w-5/7 min-h-24 max-w-26 tracking-wide z-50 justify-around'>
-                <Link href="/recipes">
-                  {isEnglish 
-                    ? <h1 className='checking pointer text-small font-medium hover:opacity-60 transform ease-in duration-100'>RECIPES</h1>
-                    : <h1 className='checking pointer text-xl font-medium hover:opacity-60 transform ease-in duration-100'>طرز تهیه غذاها</h1>
-                  }
-                </Link>
+              <div className='flex flex-col justify-center items-center w-full'>
+                <div className='flex flex-col lg:flex-row items-center text-white w-5/7 min-h-24 max-w-26 tracking-wide z-50 justify-around'>
+                  <Link href="/recipes">
+                    {isEnglish 
+                      ? <h1 className='checking pointer text-small font-medium hover:opacity-60 transform ease-in duration-100'>RECIPES</h1>
+                      : <h1 className='checking pointer text-xl font-medium hover:opacity-60 transform ease-in duration-100'>طرز تهیه غذاها</h1>
+                    }
+                  </Link>
 
-                <Link href="/about">
-                  {isEnglish 
-                    ? <h1 className='checking pointer text-small font-medium hover:opacity-60 transform ease-in duration-100'>ABOUT</h1>
-                    : <h1 className='checking pointer text-xl font-medium hover:opacity-60 transform ease-in duration-100'>درباره من</h1>
-                  }
-                </Link>
+                  <Link href="/about">
+                    {isEnglish 
+                      ? <h1 className='checking pointer text-small font-medium hover:opacity-60 transform ease-in duration-100'>ABOUT</h1>
+                      : <h1 className='checking pointer text-xl font-medium hover:opacity-60 transform ease-in duration-100'>درباره من</h1>
+                    }
+                  </Link>
 
-                <a href="/contact">
-                  {isEnglish 
-                    ? <h1 className='checking pointer text-small font-medium hover:opacity-60 transform ease-in duration-100'>CONTACT</h1>
-                    : <h1 className='checking pointer text-xl font-medium hover:opacity-60 transform ease-in duration-100'>تماس با من</h1>
-                  }
+                  <a href="/contact">
+                    {isEnglish 
+                      ? <h1 className='checking pointer text-small font-medium hover:opacity-60 transform ease-in duration-100'>CONTACT</h1>
+                      : <h1 className='checking pointer text-xl font-medium hover:opacity-60 transform ease-in duration-100'>تماس با من</h1>
+                    }
+                  </a>
+                </div>
+
+                <a href='https://www.instagram.com/theiranianvegan/' className='text-white mt-10 z-50  hover:opacity-60 transform ease-in duration-100'> 
+                  <GrInstagram  size={30}/>
                 </a>
               </div>
+              
+              <div style={{top: '50px', right: '50px', backdropFilter: 'saturate(150%) blur(20px)', backgroundColor: 'rgba(255, 255, 255, 0.8)'}} className="absolute text-sm text-gray-500 leading-none rounded-full inline-flex">
+                <button className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 ${isEnglish ? 'text-blue-400' : 'text-white'} rounded-l-full px-4 py-2`} onClick={() => setIsEnglish(true)}>
+                    <span>English</span>
+                </button>
+                <button className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 ${isEnglish ? 'text-white' : 'text-blue-400'} rounded-r-full px-4 py-2`} onClick={() => setIsEnglish(false)}>
+                    <span>فارسی</span>
+                </button>
+              </div>
 
-              <a href='https://www.instagram.com/theiranianvegan/' className='text-white mt-10 z-50  hover:opacity-60 transform ease-in duration-100'> 
-                <GrInstagram  size={30}/>
-              </a>
+
             </div>
-            
-            <div style={{top: '50px', right: '50px', backdropFilter: 'saturate(150%) blur(20px)', backgroundColor: 'rgba(255, 255, 255, 0.8)'}} className="absolute text-sm text-gray-500 leading-none rounded-full inline-flex">
-              <button className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 ${isEnglish ? 'text-blue-400' : 'text-white'} rounded-l-full px-4 py-2`} onClick={() => setIsEnglish(true)}>
-                  <span>English</span>
-              </button>
-              <button className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 ${isEnglish ? 'text-white' : 'text-blue-400'} rounded-r-full px-4 py-2`} onClick={() => setIsEnglish(false)}>
-                  <span>فارسی</span>
-              </button>
-            </div>
-
-
           </div>
+          
         </div>
         <script
           type="application/ld+json"
